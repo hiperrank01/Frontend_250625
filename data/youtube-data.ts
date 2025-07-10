a4d44828cca23f585fbb172327eb5d29ac5b9c5c
신중원 <nadasjw@gmail.com>
2025-07-10 16:28:48 +0900

    feat:유튜브영상링크연결

--- data/youtube-data.ts
+++ data/youtube-data.ts
@@ -5,34 +5,44 @@ export const youtubeCategories: Category[] = [
     title: "스케치영상",
     videos: [
       { id: "JGT0E8qMHTg", videoName: "bgf로직스" },
-      { id: "YOUTUBE_ID_2" },
+      { id: "WSGmFOu7a2w", videoName: "워라벨" },
       { id: "YOUTUBE_ID_3" },
       { id: "YOUTUBE_ID_4" },
     ],
   },
   {
-    title: "인터뷰영상",
-    videos: [{ id: "YOUTUBE_ID_10" }, { id: "YOUTUBE_ID_11" }],
-  },
-  {
-    title: "오프닝영상",
+    title: "인포그래픽",
     videos: [
-      { id: "YOUTUBE_ID_5" },
-      { id: "YOUTUBE_ID_6" },
-      { id: "YOUTUBE_ID_7" },
+      { id: "OrJiCgi6v-s", videoName: "온세카" },
+      { id: "CI5YOZnp-AU", videoName: "굿네이버스" },
+      { id: "D1Vfr6AI-SE", videoName: "에이컨트롤" },
     ],
   },
   {
-    title: "유튜브예능",
-    videos: [{ id: "YOUTUBE_ID_8" }, { id: "YOUTUBE_ID_9" }],
+    title: "오프닝영상",
+    videos: [{ id: "32t3NvDvR24", videoName: "한국언론진흥재단" }],
+  },
+  {
+    title: "인터뷰영상",
+    videos: [
+      { id: "Jy_bvLYM7eQ", videoName: "이너브랜드" },
+      { id: "Wwt-pvxlLAE", videoName: "굿네이버스_" },
+      { id: "YR_gjAPIvfU", videoName: "비케이브로스_1" },
+      { id: "bF_8kw4z8DA", videoName: "한국언론진흥재단1" },
+      { id: "xvPfTu0VE4g", videoName: "한국언론진흥재단_2" },
+    ],
   },
 
   {
-    title: "인포그래픽",
-    videos: [{ id: "YOUTUBE_ID_12" }, { id: "YOUTUBE_ID_13" }],
-  },
-  {
     title: "홍보영상",
-    videos: [{ id: "Ee9dyTW_9fQ" }, { id: "YOUTUBE_ID_15" }],
+    videos: [
+      { id: "Ee9dyTW_9fQ", videoName: "닥터차" },
+      { id: "pqjgg5E77ik", videoName: "씽긋" },
+    ],
+  },
+
+  {
+    title: "유튜브예능",
+    videos: [{ id: "YOUTUBE_ID_8" }, { id: "YOUTUBE_ID_9" }],
   },
 ];
